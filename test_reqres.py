c9148cbded883e4c32ecd5773126a0fd6ab5867d
rattus-aristarchus <lankin.m.a@gmail.com>
2023-10-29 14:27:34 +0300

    basic tests

--- test_reqres.py
+++ test_reqres.py
@@ -6,7 +6,7 @@ from utils import load_schema
 
 def test_get_users_status_code_is_ok():
     response = requests.get(url="https://reqres.in/api/users")
-    print(response.text)
+
     assert response.status_code == 200
 
 
@@ -18,11 +18,18 @@ def test_get_users_per_page():
     assert response.json()["per_page"] == 1
 
 
+def test_nonexisting_user():
+    response = requests.get(url="https://reqres.in/api/users/23")
+
+    assert response.status_code == 404
+
+
 def test_headers():
     bearer = {"Authorization": "Bearer agagwg"}
     headers = {"Connection": "keep-alive"}
     headers.update(bearer)
     response = requests.get(url="https://reqres.in/api/users", headers=headers)
+
     assert response.headers.get("connection") == "keep-alive"
     assert response.status_code == 200
 
@@ -39,3 +46,73 @@ def test_post_users_schema_validation():
 
     assert response.status_code == 201
     jsonschema.validate(response.json(), schema)
+
+
+def test_update_user():
+    response = requests.put(
+        url="https://reqres.in/api/users/2",
+        json={
+            "name": "morpheus",
+            "job": "zion resident"
+        }
+    )
+
+    assert response.status_code == 200
+    assert response.json()["job"] == "zion resident"
+
+
+def test_delete():
+    response = requests.delete(url="https://reqres.in/api/users/2")
+
+    assert response.status_code == 204
+
+
+def test_signup_success():
+    response = requests.post(
+        url="https://reqres.in/api/register",
+        json={
+            "email": "eve.holt@reqres.in",
+            "password": "pistol"
+        }
+    )
+
+    assert response.status_code == 200
+    assert response.json()["id"] == 4
+    assert response.json()["token"] == "QpwL5tke4Pnpja7X4"
+
+
+def test_signup_failure():
+    response = requests.post(
+        url="https://reqres.in/api/register",
+        json={
+            "email": "sydney@fife"
+        }
+    )
+
+    assert response.status_code == 400
+    assert response.json()["error"] == "Missing password"
+
+
+def test_login_success():
+    response = requests.post(
+        url="https://reqres.in/api/login",
+        json={
+            "email": "eve.holt@reqres.in",
+            "password": "cityslicka"
+        }
+    )
+
+    assert response.status_code == 200
+    assert response.json()["token"] == "QpwL5tke4Pnpja7X4"
+
+
+def test_login_failure():
+    response = requests.post(
+        url="https://reqres.in/api/login",
+        json={
+            "email": "peter@klaven"
+        }
+    )
+
+    assert response.status_code == 400
+    assert response.json()["error"] == "Missing password"
