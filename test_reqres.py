232d4f50a197fde01100cf6ed812b1d600ec2b61
rattus-aristarchus <lankin.m.a@gmail.com>
2023-11-25 23:07:32 +0300

    added schemas

--- test_reqres.py
+++ test_reqres.py
@@ -3,23 +3,35 @@ import requests
 
 from utils import load_schema
 
+#TODO проверку схемы ко всем тестам
 
-def test_get_users_status_code_is_ok():
-    response = requests.get(url="https://reqres.in/api/users")
+
+base_url = "https://reqres.in/api"
+
+
+def validate_schema(response, schema_name):
+    schema = load_schema(schema_name)
+    jsonschema.validate(response.json(), schema)
+
+
+def test_get_users():
+    response = requests.get(base_url+"/users")
 
     assert response.status_code == 200
+    validate_schema(response, "get_users.json")
 
 
 def test_get_users_per_page():
-    response = requests.get(url="https://reqres.in/api/users", params={"per_page": 1})
+    response = requests.get(url=base_url+"/users", params={"per_page": 1})
 
     assert response.status_code == 200
     assert len(response.json()["data"]) == 1
     assert response.json()["per_page"] == 1
+    validate_schema(response, "get_users.json")
 
 
 def test_nonexisting_user():
-    response = requests.get(url="https://reqres.in/api/users/23")
+    response = requests.get(url=base_url+"/users/23")
 
     assert response.status_code == 404
 
@@ -28,16 +40,16 @@ def test_headers():
     bearer = {"Authorization": "Bearer agagwg"}
     headers = {"Connection": "keep-alive"}
     headers.update(bearer)
-    response = requests.get(url="https://reqres.in/api/users", headers=headers)
+    response = requests.get(url=base_url+"/users", headers=headers)
 
     assert response.headers.get("connection") == "keep-alive"
     assert response.status_code == 200
+    validate_schema(response, "get_users.json")
 
 
 def test_post_users_schema_validation():
-    schema = load_schema("post_users.json")
     response = requests.post(
-        url="https://reqres.in/api/users",
+        url=base_url+"/users",
         json={
             "name": "morpheus",
             "job": "leader"
@@ -45,12 +57,12 @@ def test_post_users_schema_validation():
     )
 
     assert response.status_code == 201
-    jsonschema.validate(response.json(), schema)
+    validate_schema(response, "post_users.json")
 
 
 def test_update_user():
     response = requests.put(
-        url="https://reqres.in/api/users/2",
+        url=base_url+"/users/2",
         json={
             "name": "morpheus",
             "job": "zion resident"
@@ -58,18 +70,19 @@ def test_update_user():
     )
 
     assert response.status_code == 200
+    print(response.json())
     assert response.json()["job"] == "zion resident"
+    validate_schema(response, "put_users.json")
 
 
-def test_delete():
-    response = requests.delete(url="https://reqres.in/api/users/2")
-
+def test_delete_user():
+    response = requests.delete(url=base_url+"/users/2")
     assert response.status_code == 204
 
 
 def test_signup_success():
     response = requests.post(
-        url="https://reqres.in/api/register",
+        url=base_url+"/register",
         json={
             "email": "eve.holt@reqres.in",
             "password": "pistol"
@@ -79,11 +92,12 @@ def test_signup_success():
     assert response.status_code == 200
     assert response.json()["id"] == 4
     assert response.json()["token"] == "QpwL5tke4Pnpja7X4"
+    validate_schema(response, "post_register.json")
 
 
 def test_signup_failure():
     response = requests.post(
-        url="https://reqres.in/api/register",
+        url=base_url+"/register",
         json={
             "email": "sydney@fife"
         }
@@ -91,11 +105,12 @@ def test_signup_failure():
 
     assert response.status_code == 400
     assert response.json()["error"] == "Missing password"
+    validate_schema(response, "post_register_error.json")
 
 
 def test_login_success():
     response = requests.post(
-        url="https://reqres.in/api/login",
+        url=base_url+"/login",
         json={
             "email": "eve.holt@reqres.in",
             "password": "cityslicka"
@@ -104,11 +119,12 @@ def test_login_success():
 
     assert response.status_code == 200
     assert response.json()["token"] == "QpwL5tke4Pnpja7X4"
+    validate_schema(response, "post_login.json")
 
 
 def test_login_failure():
     response = requests.post(
-        url="https://reqres.in/api/login",
+        url=base_url+"/login",
         json={
             "email": "peter@klaven"
         }
@@ -116,3 +132,4 @@ def test_login_failure():
 
     assert response.status_code == 400
     assert response.json()["error"] == "Missing password"
+    validate_schema(response, "post_login_error.json")
